c6598bac74f6063c882a987c68abc020855ae1e7
Sanjeev Kumar <sk6208880@gmail.com>
2022-09-21 18:59:07 +0530

    second Commit

---
+++ ArithmeticOperationApi/Controllers/ValuesController.cs
@@ -0,0 +1,53 @@
+﻿using ArithmeticOperationApi;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ArithmeticOperationApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly Imath _math;
+
+        public ValuesController(Imath math)
+        {
+            _math = math;
+        }
+
+        //public Imath math { get; }
+
+        [HttpGet]
+        [Route("Addition")]
+        public ActionResult<double> additon(double a, double b)
+        {
+            double result = _math.sum(a, b);
+            return result;
+        }
+
+        [HttpGet]
+        [Route("Difference")]
+        public ActionResult<double> subtraction(double a, double b)
+        {            double result = _math.dif(a, b);
+            return result;
+        }
+
+        [HttpGet]
+        [Route("Multiplication")]
+        public ActionResult<double> multiplication(double a, double b)
+        {
+            double result = _math.mul(a, b);
+            return result;
+        }
+
+        [HttpGet]
+        [Route("Division")]
+        public ActionResult<double> division(double a, double b)
+        {
+            double result = _math.div(a, b);
+            return result;
+        }
+
+    }
+}
---
+++ ArithmeticOperationApi/Math.cs
@@ -0,0 +1,36 @@
+﻿
+
+namespace ArithmeticOperationApi
+{
+    public interface Imath
+    {
+        double sum(double a, double b);
+        double dif(double a, double b);
+        double mul(double a, double b);
+        double div(double a, double b);
+
+    }
+
+    public class Math : Imath
+    {
+        public double dif(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double div(double a, double b)
+        {
+            return a / b;
+        }
+
+        public double mul(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double sum(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
